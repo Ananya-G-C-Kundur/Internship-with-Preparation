26138ae47e928382c68191a9837425f2c7895ba4
Ananya-G-C-Kundur <ananyagckundur2004@gmail.com>
2025-09-12 11:05:12 +0530

    Coding Workshop 4

---
+++ CodingWorshop4/Pro16.java
@@ -0,0 +1,25 @@
+//Function to print unique elements in an integer array printUniqueElements
+
+public class Pro16 {
+
+    public static void printUniqueElements(int[] arr){
+        for(int readIndex = 0; readIndex<arr.length; readIndex++){
+            int compareIndex;
+            for(compareIndex = 0; compareIndex < readIndex; compareIndex++){
+                if(arr[readIndex] == arr[compareIndex]){
+                    break;
+                }
+            }
+
+            if(readIndex == compareIndex){
+                System.out.print(arr[readIndex] +" ");
+            }
+            
+        }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,4,2,4,1};
+
+        printUniqueElements(arr);
+    }
+}
